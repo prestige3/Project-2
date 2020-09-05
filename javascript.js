072a5dfa7890eb7713eafe5c3980b2c2c1d3b49e
prestige3 <67903012+prestige3@users.noreply.github.com>
2020-09-05 14:59:14 +0530

    Java for Mainproject

--- javascript.js
+++ javascript.js
@@ -1,30 +1,14 @@
-var time = 30;
 
-function printvalue(){
-console.log(time + 20);
+  function giveinput(){
+	 var inputval = document.getElementById('place').value;
+	alert( 'YOUR DESTINATION : ' + inputval);
+
+	var date1 = document.getElementById('check1').value;
+    alert("CHECK IN: " + date1);
+
+    var date2 = document.getElementById('check2').value;
+    alert("CHECK OUT: " + date2);
+
+    var dropdown = document.getElementById('choose').value;
+    alert("TRAVELERS : " + dropdown);
 }
-function changecolor(){
-	document.getElementById('tag').style.background = "red";
-    document.getElementById("tag").innerHTML = 'SPEED'
- }
- function changename(){
- 	var uservalue = document.getElementById('bet').value;
- 	var join = uservalue + "joined";
-//  	document.getElementById('gett').innerHTML = join; 
-//  var option = document.querySelector('input[name="class"]:checked').value;
-//         alert(option)
-// var values ='';
-//    var list = document.getElementsByName('select');
-//    for(i = 0 ; i < list.length ; i++){
-//    	if(list[i].checked){
-//    		values = values + list[i].value;
-//    	}
-//    }
-//    alert(values);
-var get = document.getElementById('choose').value;
- alert(get);
-
- var got = document.getElementById('year').value;
- alert(got);
-
-  }
